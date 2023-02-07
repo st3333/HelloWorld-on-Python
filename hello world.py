ccdde91e231bce136b28cf14301e988bd48a3ce3
Sergey Tytarenko <ib2718538@gmail.com>
2023-02-07 19:50:22 +0100

    first commit

---
+++ hello world.py
@@ -0,0 +1,9 @@
+name= input("Enter your name: ")
+age= str(input("Enter your age: "))
+
+print("hello " + name + " you are " + age + " years old")
+
+if age >= "18":
+    print('Test successfull')
+else:
+    print('Test failed')
---
+++ hello.py
@@ -0,0 +1,4 @@
+name=input("Enter your name: ")
+print("Hello "+name)
+age=int(input("Enter your age: "))
+prnt('you are '  c(age)+' years old')
